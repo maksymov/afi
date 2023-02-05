ed352bde3fcb8d930a33ffffe43acf09e297c647
Влад Максимов <mail@vlad85.ru>
2023-02-05 20:24:45 +0300

    commit

--- new_bot.py
+++ new_bot.py
@@ -21,8 +21,8 @@ class MyClient(discord.Client):
 
     async def setup_hook(self):
         # This copies the global commands over to your guild.
-        self.tree.copy_global_to(guild=MY_GUILD)
-        await self.tree.sync(guild=MY_GUILD)
+        #self.tree.copy_global_to(guild=MY_GUILD)
+        await self.tree.sync()
 
 intents = discord.Intents.default()
 client = MyClient(intents=intents)
